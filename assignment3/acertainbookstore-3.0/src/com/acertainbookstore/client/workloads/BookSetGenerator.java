c6cb62853cc569f34e2fd6309b0119ff7497f574
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2013-12-13 22:45:55 +0100

    book generator

--- assignment3/acertainbookstore-3.0/src/com/acertainbookstore/client/workloads/BookSetGenerator.java
+++ assignment3/acertainbookstore-3.0/src/com/acertainbookstore/client/workloads/BookSetGenerator.java
@@ -2,6 +2,7 @@ package com.acertainbookstore.client.workloads;
 
 import java.util.*;
 
+import com.acertainbookstore.business.ImmutableStockBook;
 import com.acertainbookstore.business.StockBook;
 
 /**
@@ -10,8 +11,10 @@ import com.acertainbookstore.business.StockBook;
  */
 public class BookSetGenerator {
 
+    private Random random;
+
 	public BookSetGenerator() {
-		// TODO Auto-generated constructor stub
+        random = new Random();
 	}
 
 	/**
@@ -22,7 +25,7 @@ public class BookSetGenerator {
 	 */
 	public Set<Integer> sampleFromSetOfISBNs(Set<Integer> isbns, int num) {
         List<Integer> isbnList = new ArrayList<Integer>(isbns);
-        Collections.shuffle(isbnList);
+        Collections.shuffle(isbnList, random);
         Set<Integer> resultSet = new TreeSet<Integer>();
         for (int i = 0; i < num; i++) {
             resultSet.add(isbnList.get(i));
@@ -37,7 +40,48 @@ public class BookSetGenerator {
 	 * @return
 	 */
 	public Set<StockBook> nextSetOfStockBooks(int num) {
-		return null;
+		Set<StockBook> result = new TreeSet<StockBook>();
+        for (int i = 0; i < num; i++) {
+            int isbn = Math.abs(random.nextInt());
+            String title = generateTitle();
+            String author = generateAuthor();
+            float price = Math.abs(random.nextFloat());
+            int numBooks = random.nextInt(20);
+            boolean isEditorPick = random.nextBoolean();
+
+            result.add( new ImmutableStockBook(isbn, title, author, price, numBooks, 0, 0, 0, isEditorPick) );
+        }
+
+        return result;
 	}
 
+    private String generateAuthor() {
+        String[] firstNames = new String[] {
+            "J. K.", "John", "Terry", "Neil", "Simon", "Linda", "Maurice", "Alice", "Peter", "R. L."
+        };
+        String[] lastNames = new String[] {
+            "Rowling", "Vorhaus", "Pratchett", "Gaiman", "Molineux", "Da Vinci", "DiCaprio", "Johnson", "Stine",
+            "Snowden"
+        };
+
+        return String.format("%s %s", firstNames[random.nextInt(firstNames.length)],
+                                      lastNames[random.nextInt(lastNames.length)]);
+    }
+
+    private String generateTitle() {
+        String[] firstPart = new String[] {
+            "Return of the", "Lord of the", "", "Siegfried and", "Dancing with", "So you think you can"
+        };
+        String[] secondPart = new String[] {
+            "Jedi", "King", "Mammoth", "Rings", "Pointed Stick", "Orange", "Fight", "Dance", "Whistle",
+        };
+        String[] thirdPart = new String[] {
+            "", "", "", "", "", ": The Golden Years", ": Revenge of the Mammoth", " With A Vengeance",
+            " on Holiday", ": The Official Strategy Guide", " Club"
+        };
+
+        return String.format("%s %s%s", firstPart[random.nextInt(firstPart.length)],
+                                        secondPart[random.nextInt(secondPart.length)],
+                                        thirdPart[random.nextInt(thirdPart.length)]);
+    }
 }
